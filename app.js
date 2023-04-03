cfcd5561beb9347803030b446a0573aa2090313c
Iveth Plata <karen.platahdz2223@gmail.com>
2023-04-03 08:34:17 -0600

    Descargar tipo de archivo con informacion

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'application/json'});
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+    res.writeHead(200, {'Content-Type':'application/csv'});
 
-    const persona = {
-        id: 1,
-        nombre: 'Karen'
-    }
+    res.write('id, nombre\n');
+    res.write('1, Fernando\n');
+    res.write('2, Maria\n');
+    res.write('3, Juan\n');
+    res.write('4, Pedro\n');
 
-    res.write(JSON.stringify(persona));
     res.end();
 })
 .listen(8080);
