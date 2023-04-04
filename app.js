73f707687a8524b30558441ca6c3985dfb9ede98
Iveth Plata <karen.platahdz22@outlook.es>
2023-04-03 23:22:40 -0500

    Partials para hacer componentes de codigos

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 const port = 8080;
 
+// Handlebars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use( express.static('public') );
 
-// Mandamos como argumentos este objeto
 app.get('/', function (req,res) {
     res.render('home', {
         nombre: 'Karen',
@@ -15,11 +18,17 @@ app.get('/', function (req,res) {
 });
 
 app.get('/generic', function (req,res) {
-    res.sendFile( __dirname + '/public/generic.html');
+    res.render('generic', {
+        nombre: 'Karen',
+        titulo: 'Curso de Node'
+    });
 });
 
 app.get('/elements', function (req,res) {
-    res.sendFile( __dirname + '/public/elements.html');
+    res.render('elements', {
+        nombre: 'Karen',
+        titulo: 'Curso de Node'
+    });
 });
 
 app.get('*', function (req,res) {
