f5defbd0b2f8432c8aed977dcecdef9092ac5041
Iveth Plata <karen.platahdz22@outlook.es>
2023-04-03 19:12:01 -0500

    Intro Express

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-http.createServer((req, res) => {
-    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
-    res.writeHead(200, {'Content-Type':'application/csv'});
+app.get('/', function (req,res) {
+    res.send('Hola Mundo');
+});
 
-    res.write('id, nombre\n');
-    res.write('1, Fernando\n');
-    res.write('2, Maria\n');
-    res.write('3, Juan\n');
-    res.write('4, Pedro\n');
+app.get('/home', function (req,res) {
+    res.send('Hola Mundo En Home');
+});
 
-    res.end();
-})
-.listen(8080);
+app.get('*', function (req,res) {
+    res.send('Pagina no encontrada');
+});
 
-console.log('Escuchando el puerto', 8080);
+app.listen(8080);
